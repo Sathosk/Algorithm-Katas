a04e43d955936771223a1d1e1f006ffed0241d25
Tiago <sathoskath@gmail.com>
2023-10-01 16:26:35 -0300

    added day 6 of algo

--- src/day6/TwoCrystalBalls.ts
+++ src/day6/TwoCrystalBalls.ts
@@ -1,8 +1,8 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-  const jmpAmount = Math.floor(Math.sqrt(breaks.length))
-  let i = jmpAmount
+  const jmpAmount = Math.floor(Math.sqrt(breaks.length)) // 3
+  let i = jmpAmount // 3
 
-  for (; i < breaks.length; i += jmpAmount) {
+  for (; i<breaks.length; i += jmpAmount) { // 3 < 15
     if (breaks[i]) {
       break;
     }
