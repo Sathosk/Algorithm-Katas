3731075646fd390c0d0612274a0ac2a02a2f2fbe
Tiago <sathoskath@gmail.com>
2023-10-21 19:28:25 -0300

    added day 12 algos

--- src/day12/TwoCrystalBalls.ts
+++ src/day12/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const jmpAmt = Math.floor(Math.sqrt(breaks.length));
+  let i = jmpAmt;
 
+  for (; i < breaks.length; i += jmpAmt) {
+    if (breaks[i]) break;
+  }
+
+  i -= jmpAmt;
+
+  for (; i < breaks.length; i++) {
+    if (breaks[i]) {
+      return i;
+    }
+  }
+
+  return -1;
 }
