cf88ad723b38c1e1354de5db95a2a80379a0ede4
Tiago <sathoskath@gmail.com>
2023-10-26 22:56:31 -0300

    added day 12 algos

--- src/day12/Queue.ts
+++ src/day12/Queue.ts
@@ -15,14 +15,38 @@ export default class Queue<T> {
     }
 
     enqueue(item: T): void {
+        const node = {value: item} as Node<T>;
+        this.length++;
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
 
+        this.tail.next = node;
+        this.tail = node;
     }
 
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
+        this.length--;
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        // free
+        head.next = undefined;
+
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return head.value;
     }
-    
-    peek(): T | undefined {
 
+    peek(): T | undefined {
+        return this.head?.value;
     }
 }
