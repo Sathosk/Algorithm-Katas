84e331dae1885793825b37c98ba7e37deaa129f4
Tiago <sathoskath@gmail.com>
2023-12-24 11:40:41 -0300

    added bubble sort day 16

--- src/day16/BubbleSort.ts
+++ src/day16/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      const firstValue = arr[j];
+      const secondValue = arr[j + 1];
 
+      if (secondValue < firstValue) {
+        arr[j] = secondValue;
+        arr[j + 1] = firstValue
+      }
+    }
+  }
 }
